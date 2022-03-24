4502c068e9d0236d325d23b243f657d266915d01
Carlos Eduardo Vercelino - CLVsol <carlos.vercelino@clvsol.com>
2022-03-24 17:55:58 -0300

    [ADD] clv_address_aux_l10n_br (Copied from branch 14.0)

---
+++ clv_address_aux_l10n_br/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013-Today  Carlos Eduardo Vercelino - CLVsol
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Address (Aux) - Brazilian Localization',
+    'summary': 'Address (Aux) Module - Brazilian Localization.',
+    'version': '15.0.6.0',
+    'author': 'Carlos Eduardo Vercelino - CLVsol',
+    'category': 'CLVsol Solutions',
+    'license': 'AGPL-3',
+    'website': 'https://github.com/CLVsol',
+    'images': [],
+    'depends': [
+        'clv_address_aux',
+        'clv_l10n_br_base',
+    ],
+    'data': [
+        'views/address_aux_view.xml',
+    ],
+    'demo': [],
+    'test': [],
+    'init_xml': [],
+    'test': [],
+    'update_xml': [],
+    'installable': True,
+    'application': False,
+    'active': False,
+    'css': [],
+}
